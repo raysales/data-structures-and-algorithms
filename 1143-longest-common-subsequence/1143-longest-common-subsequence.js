fbd2d253d23149735b7ceabed16f5eb2468da8fa
raysales <32973257+raysales@users.noreply.github.com>
2024-02-23 21:01:51 -0500

    Time: 101 ms (82.55%), Space: 74.2 MB (63.80%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -6,20 +6,20 @@
 var longestCommonSubsequence = function(text1, text2) {
     let n = text1.length
     let m = text2.length
-    const dp = new Array(n).fill()
-                .map(() => Array(m).fill(-1));   
+    const dp = new Array(n + 1).fill()
+                .map(() => Array(m + 1  ).fill(-1));   
     
-    return dfs(n - 1, m - 1, text1, text2, dp)
+    return dfs(n, m, text1, text2, dp)
 };
 
 const dfs = (i, j, s, t, dp) => {
     // base case
-    if (i < 0 || j < 0) return 0
+    if (i === 0 || j === 0) return 0
     
     //console.log({j})
     if (dp[i][j] !== -1) return dp[i][j]
     // chars at i & j are the same
-    if (s[i] === t[j]) 
+    if (s[i - 1] === t[j - 1]) 
         return dp[i][j] = 1 + dfs(i - 1, j - 1, s, t, dp)
     
     // chars at i & j differ
