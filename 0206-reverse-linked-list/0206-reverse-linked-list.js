2af3d86d9cacfc9602800bcf445971ef4084f0e1
raysales <32973257+raysales@users.noreply.github.com>
2024-09-30 15:29:10 -0400

    Time: 75 ms (6.87%), Space: 52.7 MB (7.53%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -20,12 +20,23 @@ var reverseList = function(head) {
     head.next = null;
     return newHead;
 */
+/*
     // either input was empty, reached the last node recur, or a single node
     if (head === null || head.next === null) return head
     
     let newHead = reverseList(head.next)
-    let next = head.next
+    let next = head.next  // rename next to front
     next.next = head
     head.next = null
     return newHead
+*/
+    const _reverseList = (head, prev) => {
+        if (!head) return prev;
+        
+        const next = head.next;
+        head.next = prev;
+        return _reverseList(next, head);
+    }
+    //let prev = null
+    return _reverseList(head, null)
 };
