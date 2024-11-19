53efb2fbec56e1cac1444cbf14facfe4748a5229
raysales <32973257+raysales@users.noreply.github.com>
2024-11-19 13:41:38 -0500

    Time: 1765 ms (5.30%), Space: 49.4 MB (94.85%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if (n <= 2) return 0;
+    
+    // 2 is the first prime so count it
+    let count = 1; 
+    
+    const isPrime = num => {
+        if (num === 2) return true;
+        if (num % 2 === 0) return false;
+        
+        for (let i = 3; i * i <= num; i += 2) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    
+    for (let i = 3; i < n; i += 2) {
+        if (isPrime(i)) count++;
+    }
+    
+    return count;
+};
