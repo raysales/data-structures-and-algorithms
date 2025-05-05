571f8982d44b9e287de15e32a8b1e0f712542524
raysales <32973257+raysales@users.noreply.github.com>
2025-05-05 16:42:59 -0400

    Time: 1 ms (31.15%) | Memory: 54.1 MB (56.08%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+/* TC = 32 or const, while SC = const
+var hammingWeight = function(n) {
+    let cnt = 0
+    let mask = 1
+    for (let i = 0; i < 32; i++) {
+        if ((n & mask) !== 0) cnt++
+        mask <<= 1
+    }
+    return cnt
+};
+*/
+// only process LSB via n & (n-1) which flips it to 0
+// while n > 0 increment cnt right away & then flip LSB
+var hammingWeight = function(n) {
+    let cnt = 0
+    while (n > 0) {
+        cnt++
+        n &= n - 1
+    }
+    return cnt
+    
+};
